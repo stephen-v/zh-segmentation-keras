2f9ff2fe7ccd629442584d8ebd2d6c93644ca0d5
wangsiyu <wangsiyu@cdwww.com>
2018-03-20 13:23:14 +0800

    readme

--- val.py
+++ val.py
@@ -1,5 +1,6 @@
 import pickle
 import lstm_model
+import pandas as pd
 
 with open('model/chars.pkl', 'rb') as inp:
     chars = pickle.load(inp)
@@ -7,6 +8,7 @@ word_size = 128
 maxlen = 32
 
 model = lstm_model.create_model(maxlen, chars, word_size)
+model.load_weights('model/model.h5', by_name=True)
 
 import re
 import numpy as np
@@ -26,18 +28,30 @@ zy = {i: np.log(zy[i]) for i in zy.keys()}
 
 
 def viterbi(nodes):
-    paths = {'b': nodes[0]['b'], 's': nodes[0]['s']}
-    for l in range(1, len(nodes)):
-        paths_ = paths.copy()
-        paths = {}
-        for i in nodes[l].keys():
-            nows = {}
-            for j in paths_.keys():
-                if j[-1] + i in zy.keys():
-                    nows[j + i] = paths_[j] + nodes[l][i] + zy[j[-1] + i]
-            k = np.argmax(nows.values())
-            paths[nows.keys()[k]] = nows.values()[k]
-    return paths.keys()[np.argmax(paths.values())]
+    paths = {'b': nodes[0]['b'], 's': nodes[0]['s']}  # 第一层，只有两个节点
+    for layer in range(1, len(nodes)):  # 后面的每一层
+        paths_ = paths.copy()  # 先保存上一层的路径
+        # node_now 为本层节点， node_last 为上层节点
+        paths = {}  # 清空 path
+        for node_now in nodes[layer].keys():
+            # 对于本层的每个节点，找出最短路径
+            sub_paths = {}
+            # 上一层的每个节点到本层节点的连接
+            for path_last in paths_.keys():
+                if path_last[-1] + node_now in zy.keys():  # 若转移概率不为 0
+                    sub_paths[path_last + node_now] = paths_[path_last] + nodes[layer][node_now] + zy[
+                        path_last[-1] + node_now]
+            # 最短路径,即概率最大的那个
+            sr_subpaths = pd.Series(sub_paths)
+            sr_subpaths = sr_subpaths.sort_values()  # 升序排序
+            node_subpath = sr_subpaths.index[-1]  # 最短路径
+            node_value = sr_subpaths[-1]  # 最短路径对应的值
+            # 把 node_now 的最短路径添加到 paths 中
+            paths[node_subpath] = node_value
+    # 所有层求完后，找出最后一层中各个节点的路径最短的路径
+    sr_paths = pd.Series(paths)
+    sr_paths = sr_paths.sort_values()  # 按照升序排序
+    return sr_paths.index[-1]  # 返回最短路径（概率值最大的路径）
 
 
 def simple_cut(s):
@@ -46,6 +60,7 @@ def simple_cut(s):
                           verbose=False)[
                 0][:len(s)]
         r = np.log(r)
+        print(r)
         nodes = [dict(zip(['s', 'b', 'm', 'e'], i[:4])) for i in r]
         t = viterbi(nodes)
         words = []
@@ -72,4 +87,5 @@ def cut_word(s):
     result.extend(simple_cut(s[j:]))
     return result
 
-print(cut_word('使得能够捕捉更长距离的信息'))
+
+print(cut_word('深度学习主要是特征学习'))
